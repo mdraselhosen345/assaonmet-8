e806d8bc0928ef31ae69552beb18f5be2a1a1fa1
md rasel islam <rashel099009@gamil.com>
2025-10-19 15:13:12 +0600

    added in file

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,16 +1,47 @@
-import React, { use } from 'react';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import starPng from '../../assets/star.png'
+import downloadPng from '../../assets/dawnload.png'
 
 
+const Card = () => {
+    const [allStors, setAllStors] = useState([]);
 
-const Card = ({filePromise}) => {
-     const data = use(filePromise);
-     console.log(data);
-     
+     useEffect(() => {
+         fetch("Filedata.json")
+         .then(res => res.json())
+         .then(data => {
+          setAllStors(data)
+         });
+      });
+
+
     return (
-        <div className='mx-auto max-w-[1150px] border-2 border-red-500 h-[800px]'>
-            <h1>dhfghg</h1>
+        <div className='mx-auto max-w-[1150px] h-[1600px] grid grid-cols-4 '>
+          {allStors.map((stors, index) => (
+            <div key={index}>
+                 <div className="card bg-base-100 w-65 shadow-sm gap-5 mt-5 pl-5">
+                     <figure>
+                       <img
+                       src={stors.image}
+                       alt="Shoes" />
+                     </figure>
+                     <div className="card-body">
+                             <h3 className='text-xl'>{stors.description}</h3>
+                        <div className="card-actions flex justify-between">
+                              <button>
+                                <img className='h-[20px] w-[20 px]' src={starPng} alt="" />{stors.ratingAvg}</button>
+                              <button><img className='h-[20px] w-[20 px]' src={downloadPng} alt="" />{stors.downloads}</button>
+                              
+                        </div>
+                     </div>
+                  </div>
+            </div>
+          ))
+            
+          }
         </div>
     );
 };
 
-export default Card;
+export default Card; 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,21 @@
-import React, { Suspense } from 'react';
+
 import Card from '../../component/Card/Card';
 import Bannar from '../../component/Bannar/Bannar';
 import Titel from '../../component/Titel/Titel';
 import Title2 from '../../component/Title2/Title2';
 
-const fetchFile = async () => {
-  const res = await fetch("/public/File.json");
-  return res.json()
-}
 
 const Home = () => {
-    const filePromise = fetchFile()
+
 
     return (
         <div>    
          <Bannar></Bannar>
          <Titel></Titel>
          <Title2></Title2>
-        <Suspense fallback={<span className="loading loading-dots loading-lg"></span>}>
-           <Card filePromise={filePromise}></Card>
-        </Suspense>
+    
+             <Card></Card>  
+         
         </div>
     );
 };
